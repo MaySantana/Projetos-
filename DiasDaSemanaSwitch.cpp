3fab834c8e6de52343d64b6efb24c074629dc37b
Mayra Santana <mayrasantana477@gmail.com>
2022-03-02 22:43:26 -0300

    Rename CódigoDiasDaSemanaSwitch.cpp to DiasDaSemanaSwitch.cpp

--- DiasDaSemanaSwitch.cpp
+++ DiasDaSemanaSwitch.cpp
@@ -6,29 +6,29 @@ int main()
 {
 	setlocale(LC_ALL, "Portuguese");
 	int semana;
-	printf("Digite um número para um dia da semana:");
+	printf("Digite um nÃºmero para um dia da semana:");
 	scanf("%i", &semana);
 	switch (semana) {
 		case 1:
 	printf("Domingo: final de semana");
 	 break;
 	    case 2:
-	printf("Segunda feira: dia útil");
+	printf("Segunda feira: dia Ãºtil");
 	 break;
 	    case 3:
-	printf("Terça-feira: dia útil");
+	printf("TerÃ§a-feira: dia Ãºtil");
 	 break;
 	    case 4:
-	printf("Quarta-feira: dia útil");
+	printf("Quarta-feira: dia Ãºtil");
 	 break;
 	    case 5:
-	printf("Quinta-feira: dia útil");
+	printf("Quinta-feira: dia Ãºtil");
 	 break;
 	    case 6:
-    printf("Sexta-feira: dia útil");
+    printf("Sexta-feira: dia Ãºtil");
      break;
         case 7:
-    printf("Sábado: fim de semana");
+    printf("SÃ¡bado: fim de semana");
      break;
 	}
 }
