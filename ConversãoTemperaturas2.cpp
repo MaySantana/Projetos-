6270a3a9a7cee92d631b4b0e0e1d2d0a5a390466
Mayra de Santana Claudino <mayrasantana477@gmail.com>
2022-10-22 19:48:41 -0300

    Update ConversãoTemperaturas2.cpp

--- ConversãoTemperaturas2.cpp
+++ ConversãoTemperaturas2.cpp
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
-//Conversão de Temperaturas
+
+//Conversão de Temperaturas de Celsius para Kelvin, C
+//May Santana, 2021
 
 int main()
 {
@@ -14,7 +16,6 @@ int main()
 	K = C + 273;
 	
 	printf("A temperatura em Kelvin é: %.2f", K);
-	
 	printf("\nEscreva a temperatura em Kelvin");
 	scanf("%f", &K);
 	
