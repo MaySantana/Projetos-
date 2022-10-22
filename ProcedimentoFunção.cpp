474a6a1992cca194d1f767b4cd92591f8cf902f6
Mayra de Santana Claudino <mayrasantana477@gmail.com>
2022-10-22 19:40:15 -0300

    Update ProcedimentoFunção.cpp

--- ProcedimentoFunção.cpp
+++ ProcedimentoFunção.cpp
@@ -1,19 +1,15 @@
+//Calculadora, C
+//May Santana, 2021
+
 #include <stdio.h>
 
-
-
 double A, B, R;
-
-
 
 void entrada(){
 
 	printf("informe o valor de A:");
-
 	scanf("%lf",&A);
-
 	printf("informe o valor de B:");
-
 	scanf("%lf",&B);
 
 }
@@ -27,9 +23,7 @@ void saida(){
 void rotSoma(){
 
 	entrada();
-
 	R = A+B;
-
 	saida();
 
 }
@@ -37,38 +31,26 @@ void rotSoma(){
 void rotSubtracao(){
 
 	entrada();
-
 	R = A-B;
-
 	saida();
 
 }
-
-
 
 void rotMultiplicacao(){
 
 	entrada();
-
 	R = A*B;
-
 	saida();
 
 }
-
-
 
 void rotDivisao(){
 
 	entrada();
-
 	R = A/B;
-
 	saida();
 
 }
-
-
 
 main(){	
 
@@ -77,47 +59,28 @@ main(){
 	while(opcao != 5){
 
 	  printf("1 - Soma\n");
-
 	  printf("2 - Subtracao\n");
-
 	  printf("3 - Multiplicacao\n");
-
 	  printf("4 - Divisao\n");
-
 	  printf("5 - Sair\n");
-
 	  printf("Digite a sua opcao:");
-
 	  scanf("%d",&opcao);
 
 	  switch(opcao){
 
 	  	case 1: rotSoma();
-
 	  	    break;  
-
 	  	case 2: rotSubtracao();
-
 	  	    break;
-
 	  	case 3: rotMultiplicacao();
-
 	  	    break;
-
 	  	case 4: rotDivisao();
-
 	  	    break;
-
 	  	case 5: printf("saindo...");
-
 	  	    break; 
-
 	  	default:printf("opcao inválida");
-
 	  	    break;
 
 	  }
-
   }
-
 }
