003c3d9da25bc48ee6db98d87c2579105517f41a
Mayra de Santana Claudino <mayrasantana477@gmail.com>
2022-10-22 19:45:11 -0300

    Update FunçãoFor.cpp

--- FunçãoFor.cpp
+++ FunçãoFor.cpp
@@ -1,3 +1,6 @@
+//Números Pares e Ímpares, C
+//May Santana, 2021
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
@@ -5,10 +8,12 @@
 main()
 {
 	setlocale(LC_ALL, "Portuguese");
+	
 	int n;
 	printf("Entre com um número: ");
 	scanf("%d", &n);
 	printf("Par\timpar\n");
+	
 	for(int count=1;count <=n;count++){
 		if(count%2 == 0)
 		printf("%d\n",count);
